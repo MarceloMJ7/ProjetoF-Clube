79d2244951b5750b913756c4b6e0738bbf9bb68c
MARCELO DE MOURA FARIA COSTA <marcelo_m_costa@estudante.sesisenai.org.br>
2025-04-09 20:21:40 -0300

    terminando validações do cadastro

--- script.js
+++ script.js
@@ -1,40 +1,61 @@
 
-
-
+    
 function validaFaleConosco() {
-    if (document.frmfaleconosco.txtnome.value == "") {
-        alert("Preencha o Campo Nome.");
+    var nome = document.frmfaleconosco.txtnome.value;
+    var expRegNome = new RegExp("^[A-zÀ-ü]{3,}([ ]{1}[A-zÀ-ü]{2,})+$");
+    if (!expRegNome.test(nome)) {
+        alert("Preencha o campo Nome Corretamente.");
         document.frmfaleconosco.txtnome.focus();
         return false;
-    } else if (document.frmfaleconosco.txtfone.value == "") {
-        alert("Preencha o Campo Telefone.");
+    }
+    var fone = document.frmfaleconosco.txtfone.value;
+    var expRegFone = new RegExp("^[0-9]{2}[0-9]{4,5}[0-9]{4}$");
+    if (!expRegFone.test(fone)) {
+        alert("Preencha o campo Telefone Corretamente.");
         document.frmfaleconosco.txtfone.focus();
         return false;
     } else if (document.frmfaleconosco.txtemail.value == "") {
         alert("Preencha seu email");
         document.frmfaleconosco.txtemail.focus();
         return false;
-    } else if (document.frmfaleconosco.cpf.value == "") {
-        alert("Preencha seu cpf");
+    }
+    var cpf = document.frmfaleconosco.cpf.value.trim();
+    var expCpf = new RegExp("^[0-9]{11}$");
+    if (cpf == "") {
+        alert("Preencha o campo CPF.");
         document.frmfaleconosco.cpf.focus();
         return false;
+    }
+
+    if (!expCpf.test(cpf)) {
+        alert("Preencha o campo CPF corretamente.");
+        document.frmfaleconosco.cpf.focus();
+        return false;
+
     } else {
         var sexoSelecionado = document.getElementsByName("sexo")
         var selecionado = false;
 
-        for(var i = 0; i < sexoSelecionado.length; i++){
-            if(sexoSelecionado[i].checked){
+        for (var i = 0; i < sexoSelecionado.length; i++) {
+            if (sexoSelecionado[i].checked) {
                 selecionado = true;
                 break;
             }
-
         }
 
-        
-        if(!selecionado){
+        if (!selecionado) {
             alert("Selecione o sexo");
             return false;
         }
+
+       if(!document.frmfaleconosco.checkbox1.checked){
+            alert("Você precisa marcar a opção para participar do clube");
+            return false;
+       }
+
     }
     return true;
+
+
 }
+
